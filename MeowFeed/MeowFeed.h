865d90716f1945dbc7e4d436abaefb49b9928e8f
grav0211 <agrav065@gmail.com>
2022-11-28 18:33:14 -0500

    Initial Commit

---
+++ MeowFeed/MeowFeed.h
@@ -0,0 +1,18 @@
+//
+//  MeowFeed.h
+//  MeowFeed
+//
+//  Created by Alexandre Gravelle on 2022-11-28.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MeowFeed.
+FOUNDATION_EXPORT double MeowFeedVersionNumber;
+
+//! Project version string for MeowFeed.
+FOUNDATION_EXPORT const unsigned char MeowFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MeowFeed/PublicHeader.h>
+
+
