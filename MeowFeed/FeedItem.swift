37ef2df4b985f3f3489224773f4c60e113fa1e1b
grav0211 <agrav065@gmail.com>
2022-11-28 18:37:47 -0500

    Create the FeedItem model

---
+++ MeowFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  MeowFeed
+//
+//  Created by Alexandre Gravelle on 2022-11-28.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: String
+    let url: URL
+    let width: Int
+    let height: Int
+}
